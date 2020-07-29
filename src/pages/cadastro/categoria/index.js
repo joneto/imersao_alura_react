47f9b9a5e2106972bfb795290aed1bbfac8af836
joneto <neto.rodrigues.14@gmail.com>
2020-07-29 14:31:38 -0300

    Aula 3 da Imersão Aulura

--- src/pages/cadastro/categoria/index.js
+++ src/pages/cadastro/categoria/index.js
@@ -1,26 +1,109 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import PageDefault from '../../../components/PageDefault';
+import FormField from '../../../components/FormField';
+
 
 function CadastroCategoria() {
+  const valoresIniciais = {
+    nome: '',
+    descricao: '',
+    cor: '#000'
+  }
+  const [categorias, setCategorias] = useState([]);
+  const [values, setValues] = useState(valoresIniciais); //Abrindo o array
+
+  function setValue(chave, valor) {
+    //chave: nome, descricao, etc.
+    setValues({
+      ...values,
+      [chave]: valor // nome: 'valor'
+    })
+  }
+
+  function handleChage(infosDoEvento) {
+    //const { getAttribute, value } = infosDoEvento.target; //extrai do 'infosDoEvento' o getAttribute e o value
+    setValue(
+      infosDoEvento.target.getAttribute('name'),
+      infosDoEvento.target.value
+    );
+  }
+
   return (
     <PageDefault>
-      <h1>Cadastro de Categoria</h1>
+      <h1>Cadastro de Categoria: {values.nome}</h1>
 
-      <form>
+      <form onSubmit={function handleSubmit(infosDoEvento) {
+        infosDoEvento.preventDefault();//parar com o reload da página
+        //console.log('Você tentou enviar o form, né?');
+        setCategorias([
+          ...categorias, //pega tudo da lista anterior 
+          values
+        ]);
 
-        <label>
-          Nome da Categoria:
+        setValues(valoresIniciais);
+      }}>
+
+        <FormField
+          label="Nome da Categoria"
+          type="text"
+          name="nome"
+          value={values.nome}
+          onChange={handleChage}
+        />
+
+        <FormField
+          label="Descrição"
+          type="text"
+          name="descricao"
+          value={values.descricao}
+          onChange={handleChage}
+        />
+
+        <FormField
+          label="Cor"
+          type="color"
+          name="cor"
+          value={values.cor}
+          onChange={handleChage}
+        />
+
+        <div>
+          <label>
+            Descrição:
+          <textarea
+              type="text"
+              value={values.descricao}
+              name="descricao"
+              onChange={handleChage}
+            />
+          </label>
+        </div>
+        <div>
+          <label>
+            Cor:
           <input
-            type="text"
-          />
-        </label>
-
+              type="color"
+              value={values.cor}
+              name="cor"
+              onChange={handleChage}
+            />
+          </label>
+        </div>
         <button>
           Cadastrar
         </button>
       </form>
 
+      <ul>
+        {categorias.map((categoria, indice) => {
+          return (
+            <li key={`${categoria}${indice}`}>
+              {categoria.nome}
+            </li>
+          )
+        })}
+      </ul>
 
       <Link to="/">
         Ir para home
